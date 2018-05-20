674b4c4f1db1c239bc2d1977c49d0cf98906f4cd
Rishabh Sharma <rishabhsharma@Rishabhs-MacBook-Pro.local>
2018-05-19 19:34:40 -0500

    Add problem #1

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # Daily-Coding-Problem
-# Daily-Coding-Problem
-# Daily-Coding-Problem
+
--- src/Problem1.java
+++ src/Problem1.java
@@ -1,4 +1,31 @@
+import java.util.HashMap;
 
+/*
+  Given a list of numbers, return whether any two sums to k.
+
+  For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+  */
 public class Problem1 {
 
+	public static void main(String[] args)
+	{
+		int[] arr = {10,15,3,7};
+		System.out.println(solution(arr,0));
+	}
+	
+	public static boolean solution(int[] arr,int sum)
+	{
+		if(arr.length == 0 || arr == null || sum == 0)
+			return true;
+		HashMap<Integer,Integer> map = new HashMap<>();
+		for(int i=0;i<arr.length;i++)
+		{
+			if(!map.containsKey(arr[i]) && sum-arr[i]>0){
+				map.put(sum-arr[i], arr[i]);
+			}
+			else if(map.containsKey(arr[i]))
+				return true;
+		}
+		return false;
+	}
 }
