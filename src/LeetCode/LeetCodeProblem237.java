55596088b41dd79bfbede5b612f9168b9ff6ccdc
Rishabh Sharma <rishabhsharma@ufl.edu>
2018-05-24 19:39:22 -0500

    Leetcode problem 237

--- src/LeetCode/LeetCodeProblem237.java
+++ src/LeetCode/LeetCodeProblem237.java
@@ -1,37 +1,16 @@
 package LeetCode;
-
-//Reverse a linked list
-public class LeetCodeProblem237{
-	public static void main(String[] args){
+// delete a element in the linked list
+public class LeetCodeProblem237 {
+	public static void main(String[] args)
+	{
 		ListNode head1 = new ListNode(1);
 		ListNode a = head1;
 		head1.next = new ListNode(3);
-		ListNode head2 = new ListNode(2);
-		ListNode b = head2;
-		head2.next = new ListNode(4);
-		ListNode r = reverse(a);
-		print(r);
+		deleteNode(a);
 	}
-	
-	public static ListNode reverse(ListNode head)
-	{
-		ListNode prev = null;
-        ListNode curr = head;
-        ListNode next = null;
-        while(curr!=null){
-            next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
-        }
-		return curr;
-	}
-	
-	public static void print(ListNode l)
-	{
-		while(l!=null){
-			System.out.println(l.val);
-			l = l.next;
-		}
-	}
+
+public static void deleteNode(ListNode node) {
+    node.val = node.next.val;
+    node.next = node.next.next;
+}
 }
